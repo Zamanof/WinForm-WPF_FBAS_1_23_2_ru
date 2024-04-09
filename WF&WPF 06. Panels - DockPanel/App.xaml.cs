f448f6a4ad78859043eba4ad793d05f382292fbb
mr13 <mr13@DESKTOP-PN0DMCN>
2024-04-09 17:23:17 +0400

    WPF Panels

---
+++ WF&WPF 06. Panels - DockPanel/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WF_WPF_06._Panels___DockPanel
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
