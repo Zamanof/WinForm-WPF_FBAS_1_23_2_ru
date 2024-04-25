229784b0a8ca42726ed473a4825686ae5e3f7815
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2024-04-25 17:52:29 +0400

    WF&WPF 11

---
+++ WF&WPF 11. Templates/DataSource.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WF_WPF_11._Templates;
+class DataSource
+{
+    private ObservableCollection<Student> students;
+    public ObservableCollection<Student> Students
+    {
+        get => students;
+    }
+
+    public DataSource()
+    {
+        students = new ObservableCollection<Student> { 
+        new Student(firstName:"Salam", lastName:"Salamzade", age:30, isActive:true),
+        new Student(firstName:"Laman", lastName:"Aslanova", age:27, isActive:true),
+        new Student(firstName:"Camal", lastName:"Babayev", age:15, isActive:false),
+        new Student(firstName:"Faiq", lastName:"Ibrahimov", age:15, isActive:false),
+        new Student(firstName:"Alina", lastName:"Orucova", age:15, isActive:true),
+        new Student(firstName:"Rasul", lastName:"Umud", age:16, isActive:false)
+        };
+    }
+}
---
+++ WF&WPF 11. Templates/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace WF_WPF_11._Templates;
+
+class Student
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public int Age { get; set; }
+    public bool IsActive { get; set; }
+
+    public Student(string? firstName, string? lastName, int age, bool isActive)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+        IsActive = isActive;
+    }
+    public override string ToString()
+    {
+        var active = IsActive ? "Yes" : "No";
+        return $"{FirstName} {LastName} - {DateTime.Now.Year - Age} Active:{active}";
+    }
+}
